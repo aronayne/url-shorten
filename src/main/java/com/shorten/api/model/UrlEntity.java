982c68f885c8e84d400d1f67aa948f9305972ab9
aronayne <adrian.ronayne@gmail.com>
2020-09-03 23:11:41 +0100

    updated java doc

--- src/main/java/com/shorten/api/model/UrlEntity.java
+++ src/main/java/com/shorten/api/model/UrlEntity.java
@@ -28,30 +28,58 @@ public class UrlEntity {
         this.shortUrl = shortUrl;
     }
 
+    /**
+     * Get the date added of the URL entity
+     * @return the date added of the URL entity
+     */
     public LocalDate getDateAdded() {
         return dateAdded;
     }
 
+    /**
+     * Set the date added of the URL entity
+     * @param dateAdded
+     */
     public void setDateAdded(LocalDate dateAdded) {
         this.dateAdded = dateAdded;
     }
 
+    /**
+     * Get the short URL of the URL entity
+     * @return
+     */
     public String getShortUrl() {
         return shortUrl;
     }
 
+    /**
+     * Set the short URL of the URL entity
+     * @return
+     */
     public void setShortUrl(String shortUrl) {
         this.shortUrl = shortUrl;
     }
 
+    /**
+     * Get the long URL of the URL entity
+     * @return
+     */
     public String getLongUrl() {
         return longUrl;
     }
 
+    /**
+     * Set the long URL of the URL entity
+     * @param longUrl
+     */
     public void setLongUrl(String longUrl) {
         this.longUrl = longUrl;
     }
 
+    /**
+     * Get the DB id of the URL entity
+     * @return DB id of the URL entity
+     */
     public Long getId() {
         return id;
     }
